b2914e8f9c021abfb1b8c33985821fafe4c77fa2
Areeves93 <32654508+Areeves93@users.noreply.github.com>
2018-02-15 20:54:28 -0500

    commit

---
+++ test2.js
@@ -0,0 +1,14 @@
+//1.What are three things you can do in order to speed up a website's load time?(20pts)
+
+1.Minify CSS 
+2.Minify HTML 
+3.Minify JavaScript
+
+//2.Go to the Google PageSpeed insights page and analyze ncl.com Choose three of the shortcomings this website has and explain the specific tools and workflows we discussed in class which could help this website address these shortcomings.(20pts)
+
+//3.What is the render tree?(20pts)
+
+A Render Tree is the combination of the DOM and CSSOM. The CSSOM and DOM trees come together to form a render tree, which is then used to compute the layout of each visible element and provides an input to the paint process that renders the pixels to the screen. Doing all of the steps mentioned is important to complete excellent rendering performance. 
+
+
+//4.Explain the process of a how a website loads in terms of the internal data structures built and used by the browser (DOM, CSSOM, etc).(20pts)
